33b746d59d5b56e57682ff094eef59d145cb5b2e
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-11 15:08:07 +0000

    commit

---
+++ src/main/java/dev/elma/hospital_spring_mvc/web/PatientController.java
@@ -0,0 +1,22 @@
+package dev.elma.hospital_spring_mvc.web;
+
+import dev.elma.hospital_spring_mvc.entities.Patient;
+import dev.elma.hospital_spring_mvc.repository.PatientRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepo patientRepo;
+    @GetMapping("/patients")
+    private String getAllPatients(Model model){
+        model.addAttribute("patientsList",patientRepo.findAll());
+        return "patients";
+    }
+
+}
