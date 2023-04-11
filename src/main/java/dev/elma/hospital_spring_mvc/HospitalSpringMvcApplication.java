cd4ba7c7d9431af0e933830cc1aee399df87a276
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-11 14:49:00 +0000

    Initial commit

---
+++ src/main/java/dev/elma/hospital_spring_mvc/HospitalSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.hospital_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/hospital_spring_mvc/HospitalSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.hospital_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
